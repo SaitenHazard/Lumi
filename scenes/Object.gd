1703c31b96a909718a7d82a92e837ea5c1ba5caf
SaitenHazard <khantowsifkhan@gmail.com>
2021-05-26 10:02:20 +0600

    05/26/2021 10:02AM

--- scenes/Object.gd
+++ scenes/Object.gd
@@ -4,6 +4,7 @@ class_name Game_object
 
 onready var Tiles = get_node('/root/Game/Tiles')
 export var coordinate : Vector2 = Vector2(5,5)
+export var direction : Vector2 = Vector2(0,-1)
 
 var left_mousebutton_pressed : bool = false
 var _offset : float 
@@ -15,6 +16,17 @@ func _ready():
 
 func _process(delta) -> void :
 	_set_position()
+	_set_direction()
+
+func _set_direction():
+	if direction == Vector2(0,-1):
+		self.rotation = deg2rad(0)
+	elif direction == Vector2(1, 0):
+		self.rotation = deg2rad(90)
+	elif direction == Vector2(0, 1):
+		self.rotation = deg2rad(180)
+	elif direction == Vector2(-1, 0):
+		self.rotation = deg2rad(-90)
 
 func _set_position() -> void :
 	if left_mousebutton_pressed == false:
@@ -24,16 +36,32 @@ func _set_position() -> void :
 		self.position =  get_viewport().get_mouse_position()
 
 func _on_Area2D_input_event(viewport, event, shape_idx) -> void:
-	if event is InputEventMouseButton \
-	and event.button_index == BUTTON_LEFT:
-		if event.is_pressed():
-			left_mousebutton_pressed = true
-		else:
-			left_mousebutton_pressed = false
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			_drag_drop(event)
 			_set_coordinate()
-			
+		elif event.button_index == BUTTON_RIGHT:
+			_change_direction(event)
+
+func _drag_drop(event) -> void:
+	if event.is_pressed():
+		left_mousebutton_pressed = true
+	else:
+		left_mousebutton_pressed = false
+
+func _change_direction(event) -> void:
+	if event.is_pressed() == false:
+		if direction == Vector2(0,-1):
+			direction = Vector2(1,0)
+		elif direction == Vector2(1, 0):
+			direction = Vector2(0,1)
+		elif direction == Vector2(0, 1):
+			direction = Vector2(-1,0)
+		else:
+			direction = Vector2(0,-1)
+
 func _set_coordinate():
-	var mouse_coordinate : Vector2
+	var mouse_coordinate : Vector2 = Vector2.ZERO
 	var mouse_position = get_viewport().get_mouse_position()
 	mouse_coordinate.x = round((mouse_position.x - _offset) / cell_size)
 	mouse_coordinate.y = round((mouse_position.y - _offset) / cell_size)
--- scenes/gun.gd
+++ scenes/gun.gd
@@ -6,8 +6,23 @@ onready var lights : Array
 onready var max_lights : float = Tiles.get_mapsize().x
 
 func _ready():
+	._ready()
 	call_deferred("_spawn_lights")
 	call_deferred("_place_lights")
+	
+func _on_Area2D_input_event(viewport, event, shape_idx) -> void:
+	._on_Area2D_input_event(viewport, event, shape_idx)
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			_manage_lights(event)
+		if event.button_index == BUTTON_RIGHT and event.is_pressed() == false:
+			_replace_lights()
+			
+func _manage_lights(event):
+	if event.is_pressed():
+		_deplace_lights()
+	else:
+		_place_lights()
 
 func _spawn_lights():
 	for i in range(max_lights):
@@ -15,14 +30,18 @@ func _spawn_lights():
 		lights.push_back(light)
 		get_node('/root/Game/Tiles').add_child(light)
 
+func _replace_lights():
+	_deplace_lights()
+	_place_lights()
+
+func _deplace_lights():
+	for i in range(lights.size()):
+		lights[i].visible = false
+
 func _place_lights():
-	var direction = Vector2(0,-1)
-	
-	for i in range(lights.size()-1):
+	for i in range(lights.size()):
 		var light_coordinate = Vector2(
 			coordinate.x + ((i+1)*direction.x), coordinate.y + ((i+1)*direction.y))
-			
 		lights[i].visible = true
-		
 		lights[i].position = Vector2(
 		_offset + light_coordinate.x * cell_size, _offset + light_coordinate.y * cell_size)
